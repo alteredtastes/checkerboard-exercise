6954262a8a49462c45dc80c2b55941088727c82d
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-02-21 15:04:59 -0800

    add flashing interval

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 var grid = [];
 var body = document.getElementsByTagName('body')[0];
 
-for (var i = 1; i <= 148; i++){
-  grid[i] = document.createElement('div');
-  grid[i].style.float = 'left';
-  grid[i].style.width = '11.1%';
-  grid[i].style.paddingBottom = '11.1%';
-
-  if((i % 2) == 0){
-    grid[i].style.background = 'red';
-  }else{
-    grid[i].style.background = 'black';
-  }
-  body.appendChild(grid[i]);
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function gridbuild() {
+  for (var i = 1; i <= 148; i++){
+    grid[i] = document.createElement('div');
+    grid[i].style.float = 'left';
+    grid[i].style.width = '11.1%';
+    grid[i].style.paddingBottom = '11.1%';
+    grid[i].style.background = 'rgb(' + getRandomIntInclusive(0, 255) + ',' + getRandomIntInclusive(0, 255) + ',' + getRandomIntInclusive(0, 255) + ')';
+    body.appendChild(grid[i]);
+}}
+
+function changeColor() {
+  for (var i = 1; i <= grid.length; i++) {
+    grid[i].style.background = 'rgb(' + getRandomIntInclusive(0, 255) + ',' + getRandomIntInclusive(0, 255) + ',' + getRandomIntInclusive(0, 255) + ')';
+  }
+}
+
+function interval(){
+  window.setInterval(changeColor, 2000);
+}
+
+gridbuild();
+interval();
