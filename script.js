f0e3409df075100d8679780a70cfcd16e2923518
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-02-21 01:32:49 -0800

    create random color grid

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var grid = [];
+var color;
 var body = document.getElementsByTagName('body')[0];
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 for (var i = 1; i <= 148; i++){
   grid[i] = document.createElement('div');
   grid[i].style.float = 'left';
   grid[i].style.width = '11.1%';
   grid[i].style.paddingBottom = '11.1%';
-
-  if((i % 2) == 0){
-    grid[i].style.background = 'red';
-  }else{
-    grid[i].style.background = 'black';
-  }
+  grid[i].style.background = 'rgb(' + getRandomIntInclusive(0, 255) + ',' + getRandomIntInclusive(0, 255) + ',' + getRandomIntInclusive(0, 255) + ')';
   body.appendChild(grid[i]);
 }
