55d3a7e7206a3131bd3fbf8fec4bb5e122ccfcbc
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-02-21 00:46:06 -0800

    create red and black checkerboard grid

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your JS goes here
+var grid = [];
+var body = document.getElementsByTagName('body')[0];
+
+for (var i = 1; i <= 148; i++){
+  grid[i] = document.createElement('div');
+  grid[i].style.float = 'left';
+  grid[i].style.width = '11.1%';
+  grid[i].style.paddingBottom = '11.1%';
+
+  if((i % 2) == 0){
+    grid[i].style.background = 'red';
+  }else{
+    grid[i].style.background = 'black';
+  }
+  body.appendChild(grid[i]);
+}
