0433f2dc3fecdae2bbcf66ff72aeac730357ca26
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-02-21 03:05:22 -0800

    add gradient

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 var grid = [];
 var body = document.getElementsByTagName('body')[0];
+var op = 0;
 
-function getRandomIntInclusive(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 for (var i = 1; i <= 148; i++){
   grid[i] = document.createElement('div');
   grid[i].style.float = 'left';
   grid[i].style.width = '11.1%';
   grid[i].style.paddingBottom = '11.1%';
-  grid[i].style.background = 'rgb(' + getRandomIntInclusive(0, 255) + ',' + getRandomIntInclusive(0, 255) + ',' + getRandomIntInclusive(0, 255) + ')';
+
+
+  if((i % 2) == 0){
+    grid[i].style.background = 'rgb(255,0,0)';
+  }else{
+    grid[i].style.background = 'rgb(0,0,0)';
+  }
+
+    op += 0.01;
+    grid[i].style.opacity = op;
+
+
   body.appendChild(grid[i]);
 }
